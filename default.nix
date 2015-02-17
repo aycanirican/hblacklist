e5937cea9cc6a3e1f607f8398313027e85bebd93
Aycan iRiCAN <iricanaycan@gmail.com>
2015-02-17 15:51:19 +0000

    generate default.nix using cabal2nix-2.0

--- default.nix
+++ default.nix
@@ -1,1 +1,17 @@
-(import ../picus-ci/picus.nix {}).hblacklist
+{ mkDerivation, attoparsec, base, hdaemonize, hinotify, network
+, network-uri, process, stdenv, text
+}:
+mkDerivation {
+  pname = "hblacklist";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  buildDepends = [
+    attoparsec base hdaemonize hinotify network network-uri process
+    text
+  ];
+  homepage = "http://github.com/aycanirican/hblacklist";
+  description = "A utility which listens snort logs and blacklists IP addresses for a certain time using iptables";
+  license = stdenv.lib.licenses.bsd3;
+}
